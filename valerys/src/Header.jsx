a67c23ce2e02434359680b05321ff4a62655fa03
Fernando <josep3194@gmail.com>
2024-06-13 18:35:52 -0500

    first commit

---
+++ valerys/src/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+const Header = () => {
+  return (
+    <header>
+       <nav className='navBar'>
+        <li>inicio</li>
+        <li>Productos</li>
+        <li>logo</li>
+        <li>nosotros</li>
+        <li>Perfil</li>
+       </nav>
+    </header>
+  )
+}
+
+export default Header
